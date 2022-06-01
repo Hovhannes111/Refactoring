2a5d5e47c1f52d8329ad31865faa4044ddc0025f
Hovhannes111 <hovhanness28@gmail.com>
2022-06-01 16:08:30 +0400

    add first test

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -1,24 +1,79 @@
 <?php
 declare(strict_types=1);
 
+use App\ApiRequests\BinlistApi;
+use App\ApiRequests\RateApi;
 use App\Classes\App;
+use App\Methods\IsEu;
 use PHPUnit\Framework\TestCase;
 
 class AppTest extends TestCase
 {
-    protected $data;
+    // protected $data;
+    protected $binlist;
+    protected $isEu;
+    protected $rate;
 
     protected function setUp() :void
     {
-        $this->data = new App();
+        // $this->data = new App();
+        $this->binlist = new BinlistApi();
+        $this->isEu = new IsEu();
+        $this->rate = new RateApi();
     }
 
     /**
      * @test
      */
-    public function checkDataTest()
+    public function testFileExists()
     {
-        $this->assertTrue(true);
+        $filename = "input.txt"; 
+        $this->assertFileExists($filename);
+        $file = file_get_contents($filename);
+        return $file;
     }
+
+    /**
+     * @test
+     * @depends testFileExists
+     */
+    public function testNotEmptyFileData($file)
+    {
+        $rows = explode("\n", $file);
+        $this->assertNotEmpty($rows);
+        // return $rows;
+    }
+
+    // /**
+    //  * @test
+    //  * @depends testNotEmptyFileData
+    //  */
+    // public function testCheckBinResults($rows)
+    // {
+    //     $arr = [];
+    //     foreach($rows as $row){
+    //         $data = json_decode($row);
+    //         $binlist = $this->binlist->getBinlist($data->bin);
+    //         $this->assertObjectHasAttribute("alpha2", $binlist->country);
+    //         $arr[] = $binlist->country->alpha2; 
+    //     }
+    //     return $arr;
+    // }
+
+    // /**
+    //  * @test
+    //  * @depends testCheckBinResults
+    //  */
+    // public function testCheckIsEu($arr)
+    // {
+    //     $this->assertNotEmpty($arr);
+    //     $boolArr = [];
+    //     foreach($arr as $elem)
+    //     {
+    //         $isEu = $this->isEu->checkIsEu($elem);
+    //         $boolArr[] = $isEu;
+    //     }
+    //     return $boolArr;
+    // }
     
 }
